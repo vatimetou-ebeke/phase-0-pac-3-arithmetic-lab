561a4118aae248ac6b4a8618a7593ef13cd08b6f
vatimetou ebeker <fatimaabcar2@gmail.com>
2021-12-28 12:11:11 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,41 +1,24 @@
-number = 6;
-numero = 4;
-var n= 10;
 
-function add() {
-  return (number+2 );
+
+function add(n,y) {
+  return (n + y );
 }
 
 
-function subtract() {
-    return (number-2 );
+function subtract(n ,y) {
+    return (n -y );
   }
 
-  function multiply() {
-    return (number*2 );
+  function multiply(n ,y) {
+    return (n*y );
   }
 
  
-  function divide() {
-    return (number/2);
+  function divide(n, y) {
+    return (n/y);
   }
 
-  function  add(number, numero) {
-    return (number+ numero);
-  }
-
-  function  subtract(number, numero) {
-    return (number- numero);
-  }
-
-  function  multiply(number, numero) {
-    return (number* numero);
-  }
-
-  function  divide(number, numero) {
-    return (number/ numero);
-  }
-
+  
   function increment(n){
     
 
@@ -47,50 +30,25 @@ function  decrement(n) {
     return (--n);
   }
 
-  //function makeInt(n) {
-    //return n;
-//}
-
-
- //function preserveDecimal(n){
-    // return NaN
- //}
+  
 
  
-    
- //=NaN
 
- //function preserveDecimal(n){
-    //return parseFloat('2.222',10)
+ function preserveDecimal(n){
+    return parseFloat(n,10)
   
-  //}
+  }
 
   function makeInt(n){
-    return parseInt("2", 10);
+    return parseInt(n, 10);
      }
     
 
- function  makeInt(n){
-    return  parseInt('n', 10)
-      
-  }
+ 
 
   
  
    
-//function makeInt(n){
-  //return parseInt('0*46.5', 10)
-    
-    //}
-    
-    
-    
-
-
-    
-       // function preserveDecimal(n){
-        //return parseFloat(n)
-        //}
 
  
 
