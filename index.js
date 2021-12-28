c24a907c6813b2c4f7bfa9949ef0742a79d55732
vatimetou ebeker <fatimaabcar2@gmail.com>
2021-12-28 11:18:04 -0500

    update

---
+++ index.js
@@ -0,0 +1,97 @@
+number = 6;
+numero = 4;
+var n= 10;
+
+function add() {
+  return (number+2 );
+}
+
+
+function subtract() {
+    return (number-2 );
+  }
+
+  function multiply() {
+    return (number*2 );
+  }
+
+ 
+  function divide() {
+    return (number/2);
+  }
+
+  function  add(number, numero) {
+    return (number+ numero);
+  }
+
+  function  subtract(number, numero) {
+    return (number- numero);
+  }
+
+  function  multiply(number, numero) {
+    return (number* numero);
+  }
+
+  function  divide(number, numero) {
+    return (number/ numero);
+  }
+
+  function increment(n){
+    
+
+    return ++n;
+  }
+
+
+function  decrement(n) {
+    return (--n);
+  }
+
+  //function makeInt(n) {
+    //return n;
+//}
+
+
+ //function preserveDecimal(n){
+    // return NaN
+ //}
+
+ 
+    
+ //=NaN
+
+ //function preserveDecimal(n){
+    //return parseFloat('2.222',10)
+  
+  //}
+
+  function makeInt(n){
+    return parseInt("2", 10);
+     }
+    
+
+ function  makeInt(n){
+    return  parseInt('n', 10)
+      
+  }
+
+  
+ 
+   
+//function makeInt(n){
+  //return parseInt('0*46.5', 10)
+    
+    //}
+    
+    
+    
+
+
+    
+       // function preserveDecimal(n){
+        //return parseFloat(n)
+        //}
+
+ 
+
+
